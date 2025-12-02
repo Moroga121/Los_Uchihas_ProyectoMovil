b856a88ef434109c1cb2bdf42406462e27b96866
Gabriel Rodríguez Mora <moroga121@gmail.com>
2025-12-02 12:12:46 -0600

    firts commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# los_uchiha_proyecto_final
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/los_uchiha_proyecto_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.los_uchiha_proyecto_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
